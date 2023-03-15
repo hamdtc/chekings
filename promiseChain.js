8391cae90a066cc8e767b40f188957e2a02b459a
hamdtc <mohamedtc73@gmail.com>
2023-03-15 06:24:43 +0530

    checking

--- promiseChain.js
+++ promiseChain.js
@@ -20,7 +20,7 @@ function greetingFnc() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve("Taking Your Order...");
-        // reject("All Waiters are busy");
+         reject("All Waiters are busy");
       }, 1000);
     });
   }
@@ -37,8 +37,8 @@ function greetingFnc() {
   function takePayment() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-       // resolve("Please pay...");
-         reject("Payment wallet balance is 0");
+      resolve("Please pay...");
+        reject("Payment wallet balance is 0");
       }, 1000);
     });
   }
@@ -57,23 +57,40 @@ function greetingFnc() {
   //   .then((res3) => takePayment())
   //   .then((res4) => thankyou());
   
-  greetingFnc()
-    .then((res1) => {
-      console.log("RES1", res1);
-      return takeOrder();
-    })
-    .then((res2) => {
-      console.log("RES2", res2);
-      return giveFood();
-    })
-    .then((res3) => {
-      console.log("RES3", res3);
-      return takePayment();
-    })
-    .then((res4) => {
-      console.log("RES4", res4);
-      return thankyou();
-    })
-    .catch((err) => {
-      console.log("ERROR>>>", err);
-    });
+  // greetingFnc()
+  //   .then((res1) => {
+  //     console.log("RES1", res1);
+  //     return takeOrder();
+  //   })
+  //   .then((res2) => {
+  //     console.log("RES2", res2);
+  //     return giveFood();
+  //   })
+  //   .then((res3) => {
+  //     console.log("RES3", res3);
+  //     return takePayment();
+  //   })
+  //   .then((res4) => {
+  //     console.log("RES4", res4);
+  //     return thankyou();
+  //   })
+  //   .catch((err) => {
+  //     console.log("ERROR>>>", err);
+  //   });
+
+  // greetingFnc().then((res1) => console.log("RES1", res1, new Date())).catch(e=>console.log(e));
+  //  takeOrder() .then((res2) => console.log("RES2", res2, new Date())).catch(e=>console.log(e));
+  //   takePayment() .then((res3) => console.log("RES3", res3, new Date())).catch(e=>console.log(e));
+
+
+  // all should be working else it wont work means give only error
+  //there is promise.any for any of them working fine
+ Promise.all([greetingFnc(), takeOrder(), takePayment()])
+  .then((res) => {
+    console.log("RES", res,new Date());
+  })
+  .catch((error) => {
+    console.log("ERROR", error);
+  });
+
+ 
